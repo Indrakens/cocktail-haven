284fd8ce3e7f386a39bdfab074cf8637ca2234c8
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-10-24 17:10:54 +0000

    Install django allauth

--- recipes/views.py
+++ recipes/views.py
@@ -8,3 +8,24 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        cocktail = get_object_or_404(queryset, slug=slug)
+        comments = cocktail.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if cocktail.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe.html",
+            {
+                "cocktail": cocktail,
+                "comments": comments, 
+                "liked": liked,
+            },
+        )    
