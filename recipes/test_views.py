7e566f048ef914f379a230c67c3f76216b559f6c
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-11-06 18:38:48 +0000

    Create test for add_cocktaol_page

--- recipes/test_views.py
+++ recipes/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import reverse
+from django.contrib.auth.models import User
 from .models import Recipe
 
 
@@ -8,12 +9,13 @@ class TestViews(TestCase):
     def test_get_home(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'index.html')   
 
     def test_get_recipe_detail_page(self):
         response = self.client.get(reverse('recipe', args=['test']))
         self.assertEqual(response.status_code, 302)
 
-          
-        
+    def test_get_add_cocktail_page(self):
+        response = self.client.get(reverse('add_cocktail'))
+        self.assertEqual(response.status_code, 302)
         
