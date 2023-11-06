14c518206a4b100f3e6c2c584a4f2d716881ef9a
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-11-06 19:16:14 +0000

    Create test for comment_form

---
+++ recipes/test_form.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from .models import Comment
+from .forms import CommentForm
+
+
+class TestCommentForm(TestCase):
+
+    def test_comment_form(self):
+        form = CommentForm()
+        self.assertEqual(form.Meta.model, Comment)
+        self.assertEqual(form.Meta.fields, ('body',)) 
